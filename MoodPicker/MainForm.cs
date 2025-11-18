9ddee68a99da8f35a45ad70dfe8a90959596eca3
AvocadoOuO <165788475+AvocadoOuO@users.noreply.github.com>
2025-11-18 18:08:06 +0800

    完成: 主頁面到選擇心情；未完成: UI圖示、做不出來的物件隨視窗大小調整位置與大小

--- MoodPicker/MainForm.cs
+++ MoodPicker/MainForm.cs
@@ -12,9 +12,55 @@ namespace MoodPicker
 {
     public partial class MainForm: Form
     {
+        // 宣告各視窗
+        private Start start;
+        private MainPage mainpage;
+        private Mode1_ChooseMode mode1_choose_mode;
+        private Mode1_EachMode mode1_each_mode;
+        private Mode2_AddList mode2_add_list;
+
         public MainForm()
         {
             InitializeComponent();
+            //設定視窗起始位置
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.WindowState = FormWindowState.Maximized;
+            panel1.Dock = DockStyle.Fill;
+
+            // 初始化各視窗
+            start = new Start();
+            mainpage = new MainPage();
+            mode1_choose_mode = new Mode1_ChooseMode();
+            mode1_each_mode = new Mode1_EachMode();
+            mode2_add_list = new Mode2_AddList();
+
+            // 監測各頁面的invoke，觸發後切換頁面
+            start.Switch_to_MainPage += () => ShowControl(mainpage);
+
+            mainpage.Switch_to_Mode1 += () => ShowControl(mode1_choose_mode);
+            mainpage.Switch_to_Mode2 += () => ShowControl(mode2_add_list);
+
+            mode1_choose_mode.Switch_to_MainPage += () => ShowControl(mainpage);
+            mode1_choose_mode.Switch_to_EachMood += (moodId) =>
+            {
+                // 傳 moodId 給下一個畫面
+                mode1_each_mode.SetMood(moodId);
+                ShowControl(mode1_each_mode);
+            };
+
+            mode1_each_mode.Switch_to_MainPage += () => ShowControl(mainpage);
+
+            // 顯示初始視窗 - Start
+            ShowControl(start);
         }
+
+        //顯示視窗
+        private void ShowControl(UserControl UC)
+        {
+            panel1.Controls.Clear();
+            UC.Dock = DockStyle.Fill;
+            panel1.Controls.Add(UC);
+        }
+
     }
 }
--- MoodPicker/Start.Designer.cs
+++ MoodPicker/Start.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MoodPicker
 {
-    partial class MainPage
+    partial class Start
     {
         /// <summary> 
         /// 設計工具所需的變數。
@@ -28,60 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("新細明體", 15F);
-            this.button1.Location = new System.Drawing.Point(250, 453);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(250, 100);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Mode1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("新細明體", 15F);
-            this.button2.Location = new System.Drawing.Point(769, 453);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(250, 100);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Mode2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 50F);
-            this.label1.Location = new System.Drawing.Point(297, 209);
+            this.label1.Location = new System.Drawing.Point(334, 296);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(623, 84);
-            this.label1.TabIndex = 3;
+            this.label1.TabIndex = 1;
             this.label1.Text = "Dilemma Solver";
             // 
-            // MainPage
+            // Start
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "MainPage";
+            this.Name = "Start";
             this.Size = new System.Drawing.Size(1280, 720);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Start_MouseClick);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+
         private System.Windows.Forms.Label label1;
     }
 }
