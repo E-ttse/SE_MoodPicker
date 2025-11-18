99f63be418ea7f8df621af309770c30128eec2d0
AvocadoOuO <165788475+AvocadoOuO@users.noreply.github.com>
2025-11-18 18:01:24 +0800

    Add files via upload
    Mode1除了EachMood頁面未做，其餘功能完成。未完成: UI圖示、做不出來的物件隨視窗大小改變調整位置與大小

---
+++ SE_MoodPicker-main/MoodPicker/Mode1_EachMode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection.Emit;
+
+namespace MoodPicker
+{
+    public partial class Mode1_EachMode: UserControl
+    {
+
+        public event Action Switch_to_MainPage;
+        private int currentMood;
+
+        public Mode1_EachMode()
+        {
+            InitializeComponent();
+        }
+
+        // 看收到哪個mood就顯示哪個mood的畫面
+        public void SetMood(int moodId)
+        {
+            currentMood = moodId;
+           
+            switch (moodId)
+            {
+                case 0:
+                    label2.Text = "Happy :D";
+                    break;
+                case 1:
+                    label2.Text = "Boring :|";
+                    break;
+                case 2:
+                    label2.Text = "Bad :(";
+                    break;
+                case 3:
+                    label2.Text = "Angry #-_-";
+                    break;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Switch_to_MainPage?.Invoke();
+        }
+    }
+}
